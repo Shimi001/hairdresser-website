9c54ae6d51108fc26710a830872febcb60d79709
Pasa <pavlo1191@gmail.com>
2025-12-28 02:06:53 +0100

    Add gallery layout with placeholders

---
+++ src/data/galleryImages.ts
@@ -0,0 +1,28 @@
+import placeholderImg from '../assets/placeholderImg.png';
+
+export const galleryImages = [
+    {
+        id: 1,
+        src: placeholderImg
+    },
+    {
+        id: 2,
+        src: placeholderImg
+    },
+    {
+        id: 3,
+        src: placeholderImg
+    },
+    {
+        id: 4,
+        src: placeholderImg
+    },
+    {
+        id: 5,
+        src: placeholderImg
+    },
+    {
+        id: 6,
+        src: placeholderImg
+    }
+];
